6fc3da04572a070190c4c71da8ab5a42aeeca454
Kurt Maurer <kurtrm@gmail.com>
2017-03-29 18:17:36 -0700

    Stuck on getting the stuff to render to the page.

--- public/scripts/controllers/aboutController.js
+++ public/scripts/controllers/aboutController.js
@@ -5,19 +5,9 @@
 
   aboutController.index = () => {
     $('#about').show().siblings().hide(); // REVIEW: We have a slight refactor in selectors here, which has reduced the amount of code from the last lab.
-    $.ajax({
-      url: 'https://api.github.com/user/repos?type=owner',
-      method: 'GET',
-      headers: {
-        Authorization:`token ${githubToken}`
-      }
-    })
-    .then(
-      data => data.map(repo => $('#about').append(`<p>${repo}</p>`))
-    )
+    repos.requestRepos(repoView.index);
     // TODO: Call a function to load all the data.
     // Pass a view function as a callback, so the view will render after the data is loaded.
-
   };
 
   module.aboutController = aboutController;
--- public/scripts/views/repoView.js
+++ public/scripts/views/repoView.js
@@ -11,12 +11,14 @@
     $about.show().siblings().hide();
   };
 
+  let render = Handlebars.compile($('#repo-template').text());
+
   // TODO: Remember that new Handlebars template? Let's compile it!
   // Save the result in this `render` variable.
 
   repoView.index = function() {
+    console.log('');
     ui();
-
     // The jQuery `append` method lets us append an entire array of HTML elements at once:
     $('#about ul').append(
       repos.with('name').map(render) // Want to filter by a different property other than name?
